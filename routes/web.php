3b014c03f92762f9eeb2ef8de0bd263f90b4cc32
Mai Viết Dũng <no_body156@yahoo.com.vn>
2023-01-14 01:02:08 +0700

    update sitemap

--- routes/web.php
+++ routes/web.php
@@ -137,6 +137,7 @@ Route::any('/ckfinder/browser', '\CKSource\CKFinderBridge\Controller\CKFinderCon
     | Webpage Routes
     |--------------------------------------------------------------------------
 */
+Route::get("/sitemap.xml", "SitemapController@index");
 
 Route::prefix("/")->group(function (){
    Route::get("/", "WebPageController@indexHome")->name("webPageIndex");
@@ -158,5 +159,4 @@ Route::prefix("/")->group(function (){
 
     Route::get("/download/{id}", "WebPageController@download");
     Route::get("/dl/download", "WebPageController@dlDownload")->name("dlDownload");
-
 });
